bdcc26b0582f14fa82c18eafdff5936c37b73681
Madhavi-Karnati <108082421+Madhavi-Karnati@users.noreply.github.com>
2022-06-23 21:28:45 +0530

    Add files via upload

---
+++ unity_injector/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Unity;
+namespace DIUsingUnity
+{
+    public interface Ipayment
+    {
+        void display();
+    }
+    public class GpayL : Ipayment
+    {
+        public void display()
+        {
+            Console.WriteLine("You are using gpay");
+        }
+    }
+    public class Phonepe : Ipayment
+    {
+        public void display()
+        {
+            Console.WriteLine("you are using phonepe");
+        }
+    }
+    public class Customer
+    {
+        private Ipayment obj;
+        public Customer(Ipayment dalobj)
+        {
+            obj = dalobj;
+        }
+        public void display()
+        {
+            obj.display();
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            IUnityContainer unityContainer = new UnityContainer();
+            unityContainer.RegisterType<Customer>();
+            unityContainer.RegisterType<Ipayment, Phonepe>();
+            //unityContainer.RegisterType<Ipayment, OracleDAL>();
+            Customer customer = unityContainer.Resolve<Customer>();
+            customer.display();
+            Console.ReadLine();
+        }
+    }
+}
