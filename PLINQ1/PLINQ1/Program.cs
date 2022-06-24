19569633f4d7dbbf3e45f2c33191ddcf9236c05f
Madhavi-Karnati <108082421+Madhavi-Karnati@users.noreply.github.com>
2022-06-24 08:09:54 +0530

    Add files via upload

---
+++ PLINQ1/PLINQ1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+namespace ParallelLINQDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var numbers = Enumerable.Range(1, 20);
+
+            var evenNumbers = numbers.Where(x => x % 2 == 0).ToList();
+            Console.WriteLine("Even Numbers Between 1 and 20");
+            foreach (var number in evenNumbers)
+            {
+                Console.WriteLine(number);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
